f341e80c48ceb8322ef00c2b8b81c8c3ea02a84f
Cédric Sauriol <cedric.sauriol@hotmail.com>
2019-12-03 22:43:54 -0500

    Day 03 Part 02.

--- AdventOfCode/AdventOfCode/Solvers/Day03/Day03Solver.cs
+++ AdventOfCode/AdventOfCode/Solvers/Day03/Day03Solver.cs
@@ -9,30 +9,8 @@ namespace AdventOfCode.Solvers {
     public string SolvePartOne(string[] input) {
       List<Point> wire1Points = ParsePoints(input[0]);
       List<Point> wire2Points = ParsePoints(input[1]);
-      List<Point> intersections = new List<Point>();
+      List<Point> intersections = GetIntersections(wire1Points, wire2Points);     
 
-      Point p1;
-      Point p2;
-      Point q1;
-      Point q2;
-
-      SegmentsIntersectionVerificator siv = new SegmentsIntersectionVerificator();
-      SegmentsIntersectionCalculator sic = new SegmentsIntersectionCalculator();
-
-      for(int i = 0; i < wire1Points.Count - 1; i++) {
-        p1 = wire1Points[i];
-        p2 = wire1Points[i + 1];
-
-        for(int j = 0; j < wire2Points.Count - 1; j++) {
-          q1 = wire2Points[j];
-          q2 = wire2Points[j + 1];
-
-          if(siv.Verify(p1, p2, q1, q2)) {
-            intersections.Add(sic.Calculate(p1, p2, q1, q2));
-          }
-        }
-      }
-      
       int lowestDistance = Int32.MaxValue;
       foreach (Point p in intersections) {
         lowestDistance = Math.Min(lowestDistance, Math.Abs(p.X) + Math.Abs(p.Y));
@@ -42,7 +20,20 @@ namespace AdventOfCode.Solvers {
     }
 
     public string SolvePartTwo(string[] input) {
-      return 1.ToString();
+      List<Point> wire1Points = ParsePoints(input[0]);
+      List<Point> wire2Points = ParsePoints(input[1]);
+      List<Point> intersections = GetIntersections(wire1Points, wire2Points);     
+
+      int d1 = 0;
+      int d2 = 0;
+      int lowestSteps = Int32.MaxValue;
+      foreach (Point p in intersections) {
+       d1 = GetWireStepsFromIntersection(p, wire1Points);
+       d2 = GetWireStepsFromIntersection(p, wire2Points);
+       lowestSteps = Math.Min(lowestSteps, d1 + d2);
+      }
+
+      return lowestSteps.ToString();
     }
 
     private List<Point> ParsePoints(string wirePath) {
@@ -85,6 +76,70 @@ namespace AdventOfCode.Solvers {
 
     private void ActionAlterCoordinatesRight(int[] coordinates, int steps) {
       coordinates[0] += steps;
+    }
+
+    private List<Point> GetIntersections(List<Point> wire1, List<Point> wire2) {
+      List<Point> intersections = new List<Point>();
+
+      Point p1;
+      Point p2;
+      Point q1;
+      Point q2;
+
+      SegmentsIntersectionVerificator siv = new SegmentsIntersectionVerificator();
+      SegmentsIntersectionCalculator sic = new SegmentsIntersectionCalculator();
+
+      for(int i = 0; i < wire1.Count - 1; i++) {
+        p1 = wire1[i];
+        p2 = wire1[i + 1];
+
+        for(int j = 0; j < wire2.Count - 1; j++) {
+          q1 = wire2[j];
+          q2 = wire2[j + 1];
+
+          if(siv.Verify(p1, p2, q1, q2)) {
+            intersections.Add(sic.Calculate(p1, p2, q1, q2));
+          }
+        }
+      }
+
+      return intersections;
+    }
+
+    private int GetWireStepsFromIntersection(Point q, List<Point> points) {
+      int i = 0;
+      int steps = 0;
+
+      Point p1 = points[i];
+      Point p2 = points[i + 1];
+      while (i < points.Count - 1 && false == IsIntersectionOnSegment(q, p1, p2)) {
+         steps += Math.Abs(p2.X - p1.X) + Math.Abs(p2.Y - p1.Y);
+         i++;
+         p1 = points[i];
+         p2 = points[i + 1];
+      }
+
+      return steps + Math.Abs(q.X - p1.X) + Math.Abs(q.Y - p1.Y);
+    }
+
+    private bool IsIntersectionOnSegment(Point q, Point p1, Point p2) {
+      if (q.X == p1.X && q.X == p2.X && p1.Y < q.Y && q.Y < p2.Y) {
+        return true;
+      }
+      
+      if (q.X == p1.X && q.X == p2.X && p1.Y > q.Y && q.Y > p2.Y) {
+        return true;
+      }
+      
+      if (q.Y == p1.Y && q.Y == p2.Y && p1.X < q.X && q.X < p2.X) {
+        return true;
+      }
+      
+      if (q.Y == p1.Y && q.Y == p2.Y && p1.X > q.X && q.X > p2.X) {
+        return true;
+      }
+
+      return false;
     }
   }
 }
